3f2c4adc5aa094905be80fbf09e6199260257725
victor-singha <vicunite2@gmail.com>
2021-04-14 09:56:36 +0530

    [vic] updated code

--- Reverse.java
+++ Reverse.java
@@ -1,7 +1,11 @@
 class Reverse{
 	public static void main(String args[]){
-		//reverse a Number
-		int number = 648;
+		reverse_number(648);
+		reverse_string("apple");		
+	}
+
+//reverse a Number Function
+	static void reverse_number(int number){
 		int reverse = 0;
 		while(number!=0){
 			int digit = number % 10;
@@ -9,9 +13,10 @@ class Reverse{
 			number = number / 10;
 		}
 		System.out.println(reverse);
-
-		//reverse a String
-		String st = "apple";
+	}
+	
+//reverse a String Function
+	static void reverse_string(String st){
 		char[] arr = st.toCharArray();
 		for(int i=arr.length-1;i>=0;i--){
 			System.out.print(arr[i]);
