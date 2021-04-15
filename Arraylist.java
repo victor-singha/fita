0a6329986c6fd3a2c382cf248fbe45f7bf3a4c55
victor-singha <vicunite2@gmail.com>
2021-04-15 11:05:38 +0530

    arraylist initialize

---
+++ Arraylist.java
@@ -0,0 +1,10 @@
+import java.util.*;
+
+class Arraylist {
+    public static void main(String args[]) {
+        ArrayList<String> language = new ArrayList<String>(Arrays.asList("java", "python", "c", "c++", "php"));
+        for (String i : language) {
+            System.out.println(i);
+        }
+    }
+}
