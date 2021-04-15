46269b3eb3617082fe97a549427dfcfcd097dca5
victor-singha <vicunite2@gmail.com>
2021-04-15 11:28:07 +0530

    array list updated

--- Arraylist.java
+++ Arraylist.java
@@ -2,10 +2,21 @@ import java.util.*;
 
 class Arraylist {
     public static void main(String args[]) {
+        // initialise ArrayList
         ArrayList<String> language = new ArrayList<String>(Arrays.asList("java", "python", "c", "c++", "php"));
+        // add data
+        language.add("javascript");
+        language.add("ruby");
+        language.add("c#");
         for (String i : language) {
+            // get data without index
             System.out.println(i);
         }
+        for (int i = 0; i < language.size(); i++) {
+            // get data from ArrayList through index
+            System.out.println(language.get(i));
+        }
+        // print whole ArrayList
         System.out.println(language);
     }
 }
