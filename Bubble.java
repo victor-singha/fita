bd88f0f48a5adb85dceaba9d8bf32ab70b279374
victor-singha <vicunite2@gmail.com>
2021-04-16 21:34:35 +0530

    bubbleSort code

--- Bubble.java
+++ Bubble.java
@@ -7,7 +7,7 @@ class Bubble {
         int counter = 1;
         while (counter < n) {
             for (int i = 0; i < n - counter; i++) {
-                if (arr[i] > arr[i + 1]) {
+                if (arr[i] > arr[i + 1]) { // for descending order arr[i] < arr[i+1]
                     int temp = arr[i];
                     arr[i] = arr[i + 1];
                     arr[i + 1] = temp;
