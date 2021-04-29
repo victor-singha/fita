40a0b5fa02f42ee3778dfe2b087473dff9ee1ffa
victor-singha <vicunite2@gmail.com>
2021-04-29 15:33:05 +0530

    012343210

---
+++ Zero_One_Zero.java
@@ -0,0 +1,19 @@
+public class Zero_One_Zero {
+    public static void main(String[] args) {
+
+        // input 3
+        // output 0123210
+        // input 5
+        // output 01234543210
+        int n = 3, j = 1;
+        for (int i = 0; i <= n * 2; i++) {
+            if (i <= n) {
+                System.out.println(i);
+            } else {
+                System.out.println(n - j);
+                j++;
+            }
+        }
+    }
+
+}
