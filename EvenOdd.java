b4caf77097a86a9562bf6cbab58786f033fa3739
victor-singha <vicunite2@gmail.com>
2021-05-23 09:00:18 +0530

    EvenOdd

---
+++ EvenOdd.java
@@ -0,0 +1,22 @@
+import java.util.LinkedList;
+
+public class EvenOdd {
+    public static void main(String[] args) {
+        LinkedList<Integer> list = new LinkedList<Integer>();
+        LinkedList<Integer> sorted = new LinkedList<Integer>();
+        list.add(5);
+        list.add(10);
+        list.add(3);
+        list.add(4);
+        list.add(7);
+        for (Integer i : list) {
+            if (i % 2 == 0) {
+                sorted.addFirst(i);
+            } else
+                sorted.addLast(i);
+        }
+        for (Integer x : sorted) {
+            System.out.println(x);
+        }
+    }
+}
