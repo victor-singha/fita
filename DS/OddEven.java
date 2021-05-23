c44d3814c132f67a4e112a78d079db819978d4d2
victor-singha <vicunite2@gmail.com>
2021-05-23 16:46:56 +0530

    oddeven leet code

---
+++ DS/OddEven.java
@@ -0,0 +1,29 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+package DS;
+
+class OddEven {
+    public Node<Integer> oddEvenList(Node<Integer> head) {
+        if (head == null)
+            return null;
+        Node<Integer> odd = head;
+        Node<Integer> even = head.next;
+        Node<Integer> evenHead = even;
+        while (even != null && even.next != null) {
+            odd.next = even.next;
+            odd = odd.next;
+            even.next = odd.next;
+            even = even.next;
+        }
+        odd.next = evenHead;
+        return head;
+    }
+}
